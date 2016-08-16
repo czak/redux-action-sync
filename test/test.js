b855625bbeb9dac0f286a748328ddbde5a249d13
Łukasz Adamczak <lukasz@czak.pl>
2016-08-16 15:01:19 +0200

    More tests on the current middleware behavior

--- test/test.js
+++ test/test.js
@@ -2,22 +2,60 @@ import expect from 'expect';
 import createActionSync from '../src';
 
 describe('redux-action-sync', () => {
-  before(() => {
-    global.localStorage = {
-      getItem() {},
-      setItem() {},
-    };
-  });
-
   describe('middleware', () => {
-    let middleware;
+    const action = { type: 'TEST_ACTION' };
 
-    beforeEach(() => {
-      middleware = createActionSync(() => Promise.resolve());
+    before(() => {
+      global.localStorage = {
+        getItem() {},
+        setItem() {},
+      };
+    });
+
+    context('without localStorage.actionCount', () => {
+      before(() => {
+        global.localStorage = {
+          getItem() {},
+          setItem: expect.createSpy(),
+        };
+      });
+
+      it("calls 'push' with index == 0", () => {
+        const push = expect.createSpy().andReturn(Promise.resolve());
+        createActionSync(push)()(() => {})(action);
+        expect(push).toHaveBeenCalledWith(0, action);
+      });
+
+      it('sets localStorage.actionCount to 1', () => {
+        const push = expect.createSpy().andReturn(Promise.resolve());
+        createActionSync(push)()(() => {})(action);
+        expect(localStorage.setItem).toHaveBeenCalledWith('actionCount', 1);
+      });
+    });
+
+    context('with localStorage.actionCount', () => {
+      before(() => {
+        global.localStorage = {
+          getItem() { return '12'; },
+          setItem: expect.createSpy(),
+        };
+      });
+
+      it("calls 'push' with index == localStorage.actionCount", () => {
+        const push = expect.createSpy().andReturn(Promise.resolve());
+        createActionSync(push)()(() => {})(action);
+        expect(push).toHaveBeenCalledWith(12, action);
+      });
+
+      it('increments localStorage.actionCount by 1', () => {
+        const push = expect.createSpy().andReturn(Promise.resolve());
+        createActionSync(push)()(() => {})(action);
+        expect(localStorage.setItem).toHaveBeenCalledWith('actionCount', 13);
+      });
     });
 
     it('dispatches the action', () => {
-      const action = { type: 'TEST_ACTION', value: 123 };
+      const middleware = createActionSync(() => Promise.resolve());
       const dispatch = expect.createSpy();
       return middleware()(dispatch)(action).then(() => {
         expect(dispatch).toHaveBeenCalledWith(action);
