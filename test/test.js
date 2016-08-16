a26c2a5a36d3d31548bae5c09004ce6fce048a62
Łukasz Adamczak <lukasz@czak.pl>
2016-08-16 13:59:54 +0200

    First real test for the middleware

--- test/test.js
+++ test/test.js
@@ -1,7 +1,27 @@
 import expect from 'expect';
+import createActionSync from '../src';
 
-describe('true', () => {
-  it('should be true', () => {
-    expect(true).toEqual(true);
+describe('redux-action-sync', () => {
+  before(() => {
+    global.localStorage = {
+      getItem() {},
+      setItem() {},
+    };
+  });
+
+  describe('middleware', () => {
+    let middleware;
+
+    beforeEach(() => {
+      middleware = createActionSync(() => Promise.resolve());
+    });
+
+    it('dispatches the action', () => {
+      const action = { type: 'TEST_ACTION', value: 123 };
+      const dispatch = expect.createSpy();
+      return middleware()(dispatch)(action).then(() => {
+        expect(dispatch).toHaveBeenCalledWith(action);
+      });
+    });
   });
 });
