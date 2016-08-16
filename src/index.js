6470c2946f8374d6b058595f8f45997873c5d3b6
Łukasz Adamczak <lukasz@czak.pl>
2016-08-16 21:44:09 +0200

    Retry on rejected push

--- src/index.js
+++ src/index.js
@@ -4,16 +4,23 @@ const getActionCount = () =>
 const incrementActionCount = () =>
   localStorage.setItem('actionCount', getActionCount() + 1);
 
-export default push => () => next => action =>
-  push(getActionCount(), action).then(
-    () => {
-      next(action);
-      incrementActionCount();
-    },
-    error => {
-      error.conflicts.forEach(conflict => {
-        next(conflict);
+const createActionSync = push => () => next => action => {
+  const process = () =>
+    push(getActionCount(), action).then(
+      () => {
+        next(action);
         incrementActionCount();
-      });
-    }
-  );
+      },
+      error => {
+        error.conflicts.forEach(conflict => {
+          next(conflict);
+          incrementActionCount();
+        });
+        return process();
+      }
+    );
+
+  return process();
+};
+
+export default createActionSync;
--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ global.localStorage = {
   setItem(key, value) { this[key] = value; },
 };
 
-const createError = (data) => Object.assign(new Error(), data);
+const createError = (conflicts) => Object.assign(new Error(), { conflicts });
 
 describe('redux-action-sync', () => {
   beforeEach(() => delete localStorage.actionCount);
@@ -55,24 +55,30 @@ describe('redux-action-sync', () => {
     });
 
     context('on conflict', () => {
-      const conflicts = [{ type: 'CONFLICT' }, { type: 'ANOTHER' }];
+      const conflicts1 = [{ type: 'CONFLICT' }, { type: 'ANOTHER' }];
+      const conflicts2 = [{ type: 'YET_ANOTHER' }];
 
-      it('dispatches all conflicting actions', () => {
-        const error = createError({ conflicts });
-        const middleware = createActionSync(() => Promise.reject(error));
+      it('dispatches all conflicting actions and retries', () => {
+        // Mock 'push' implementation fails twice with a conflict
+        // and resolves on the third attempt.
+        // This is to verify the retry mechanism of the middleware.
+        let attempt = 0;
+        const push = () => {
+          attempt++;
+          if (attempt === 1) return Promise.reject(createError(conflicts1));
+          else if (attempt === 2) return Promise.reject(createError(conflicts2));
+          return Promise.resolve();
+        };
+
+        const middleware = createActionSync(push);
         const dispatch = expect.createSpy();
         return middleware()(dispatch)(action).then(() => {
-          expect(dispatch.calls.length).toEqual(2);
-          expect(dispatch.calls[0].arguments[0]).toEqual(conflicts[0]);
-          expect(dispatch.calls[1].arguments[0]).toEqual(conflicts[1]);
-        });
-      });
-
-      it('increments actionCount for each conflict dispatched', () => {
-        const error = createError({ conflicts });
-        const middleware = createActionSync(() => Promise.reject(error));
-        return middleware()(() => {})(action).then(() => {
-          expect(localStorage.actionCount).toEqual(2);
+          expect(dispatch.calls.length).toEqual(4);
+          expect(dispatch.calls[0].arguments[0]).toEqual(conflicts1[0]);
+          expect(dispatch.calls[1].arguments[0]).toEqual(conflicts1[1]);
+          expect(dispatch.calls[2].arguments[0]).toEqual(conflicts2[0]);
+          expect(dispatch.calls[3].arguments[0]).toEqual(action);
+          expect(localStorage.actionCount).toEqual(4);
         });
       });
     });
