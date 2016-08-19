589eea645cbfc764d5404bb23b56f0e192b615d3
Łukasz Adamczak <lukasz@czak.pl>
2016-08-19 21:40:03 +0200

    Drop localStorage and configure the example

--- example/store/configureStore.js
+++ example/store/configureStore.js
@@ -1,8 +1,23 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
+import createActionSync from '../../lib'
+import axios from 'axios';
+
+const push = (index, action) => {
+  return axios.post('/actions', { index, action })
+       .catch(error => {
+         throw Object.assign(error, { conflicts: error.response.data });
+       })
+}
 
 export default function configureStore(preloadedState) {
-  const store = createStore(rootReducer, preloadedState)
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(
+      createActionSync(push)
+    )
+  )
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
-const getActionCount = () =>
-  parseInt(localStorage.getItem('actionCount'), 10) || 0;
+const createActionSync = push => {
+  let actionCount = 0;
 
-const incrementActionCount = () =>
-  localStorage.setItem('actionCount', getActionCount() + 1);
+  return () => next => action => {
+    const process = () =>
+      push(actionCount, action).then(
+        () => {
+          next(action);
+          actionCount++;
+        },
+        error => {
+          error.conflicts.forEach(conflict => {
+            next(conflict);
+            actionCount++;
+          });
+          return process();
+        }
+      );
 
-const createActionSync = push => () => next => action => {
-  const process = () =>
-    push(getActionCount(), action).then(
-      () => {
-        next(action);
-        incrementActionCount();
-      },
-      error => {
-        error.conflicts.forEach(conflict => {
-          next(conflict);
-          incrementActionCount();
-        });
-        return process();
-      }
-    );
-
-  return process();
-};
+    return process();
+  };
+}
 
 export default createActionSync;
